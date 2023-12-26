1c451203cb6e12b511a1633b6dd24e4afd358cb4
You_5C24 <youlongte@fhcn.cn>
2023-12-26 08:48:15 +0800

    feat: pinia+tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-pink-500">sokoban{{ map }}</div>
+</template>
+
+<script setup lang="ts">
+import { useMapStore } from "./store/map.ts";
+
+const { map } = useMapStore();
+</script>
